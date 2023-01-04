2d5cb3f113355f2873935eb08b6c63151bdd480b
Felipe Moyano <gafemoyano@gmail.com>
2023-01-04 00:20:53 -0500

    day 7 done!

--- 2022/07/solution.rb
+++ 2022/07/solution.rb
@@ -8,7 +8,73 @@ class Tree
 
   def initialize
     @root = Directory.create_root
-    @current_dir = @root.name
+    @current_dir = @root
+  end
+
+  def change_dir(name)
+    self.current_dir = case name
+                       when '/'
+                         @root
+                       when '..'
+                         current_dir.parent
+                       else
+                         find_dir(name)
+                       end
+  end
+
+  def create_dir(name)
+    current_dir.create_dir(name)
+  end
+
+  def create_file(name, size)
+    current_dir.create_file(name, size)
+  end
+
+  def directories
+    current_dir.directories
+  end
+
+  def files
+    current_dir.files
+  end
+
+  def find_dir(name)
+    raise StandardError, 'that method doesnt exist' unless (dir = directories.find { _1.name == name })
+
+    dir
+  end
+
+  def ls
+    current_dir.children.map(&:ls).join
+  end
+
+  def print
+    root.print
+  end
+
+  def all_directories
+    root.all_directories
+  end
+
+  def total_size
+    all_directories.select { _1.size < 100_000 }.map(&:size).sum
+  end
+
+  def update
+    used_space = 70_000_000 - root.size
+
+    missing_space = 30_000_000 - 27_963_297
+
+    p all_directories.map(&:size).filter { _1 >= missing_space }.sort
+  end
+
+  def traverse
+    root.children.reduce(0) do |sum, child|
+      next sum if child.is_a? SimpleFile
+
+      child.children
+      sum + child.size
+    end
   end
 end
 
@@ -19,13 +85,23 @@ class Node
     @name = name
     @parent = parent
   end
+
+  def ls
+    puts name
+  end
 end
 
 class SimpleFile < Node
+  attr_accessor :size
+
   def initialize(name, parent, size)
-    @size = size
+    @size = size.to_i
 
     super(name, parent)
+  end
+
+  def print
+    puts "F: #{name}"
   end
 end
 
@@ -33,7 +109,7 @@ class Directory < Node
   attr_accessor :children
 
   def initialize(name, parent)
-    @root = []
+    @children = []
 
     super(name, parent)
   end
@@ -41,31 +117,104 @@ class Directory < Node
   def self.create_root
     Directory.new('/', nil)
   end
+
+  def create_dir(name)
+    children << Directory.new(name, self)
+  end
+
+  def create_file(name, size)
+    children << SimpleFile.new(name, self, size)
+  end
+
+  def files
+    children.filter { |child| child.instance_of? SimpleFile }
+  end
+
+  def directories
+    children.filter { |child| child.instance_of? Directory }
+  end
+
+  def all_directories
+    return self if directories.empty?
+
+    directories.flat_map(&:all_directories).prepend(self)
+  end
+
+  def size
+    children.map(&:size).sum
+  end
+
+  def print
+    puts "DIR: #{name}"
+    children.each { |child| child.print }
+  end
 end
 
 fs = Tree.new
 
-def parse_line(line)
+input.each_line do |line|
   ary = line.split(' ')
   case ary
-  in ['$', 'cd', '..']
-    'CHANGE DIR'
-
   in ['$', 'cd', dir]
-    'CHANGE DIR'
+    fs.change_dir(dir)
+  in ['$', 'ls']
+    next
+  in ['dir', name]
+    fs.create_dir(name)
+  in [size, name]
+    fs.create_file(name, size)
   else
-    if line.match(/(?<size>\d+)\s(?<name>\w+)/)
-
-    end
+    puts 'WHAT THE FUCK'
   end
 end
 
-input.each_line do |_line|
-  asdf = []
-end
+p '----SOLUTION 1-------'
+p fs.total_size
 
-class SolutionTest < Minitest::Test
-  def test_parse_command
-    assert_equal parse_line('$ cd dir'), 'CHANGE DIR'
+p '----SOLUTION 2-------'
+p fs.update
+
+directories = Hash.new { |h, k| h[k] = [] }
+
+current_dir = []
+
+DATA.each_line do |line|
+  case line.split
+  in [/\d+/ => size, _file]
+    directories[current_dir.clone] << size.to_i
+  in ['dir', /\w+/ => dir]
+    new_dir = current_dir.clone.append(dir)
+    directories[current_dir.clone] << directories[new_dir]
+  in ['$', 'cd', '..']
+    current_dir.pop
+  in ['$', 'cd', /.+/ => dir]
+    current_dir << dir
+  else # ls
   end
 end
+
+
+__END__
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
